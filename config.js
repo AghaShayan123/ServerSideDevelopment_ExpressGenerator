77886f9a7c5aaabc943a89c02ba364a827d28789
unknown <agha.shayan123@gmail.com>
2021-07-28 20:07:25 +0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '960276641486941',
+        clientSecret: '2948a9a108ae71a98aa01fe43102fc9b'
+    }
 }
